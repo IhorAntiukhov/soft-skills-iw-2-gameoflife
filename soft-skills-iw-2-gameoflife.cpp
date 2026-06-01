e89e0be492e0c7c1415a48e6352000fd96c91fc6
IhorAntiukhov <holaborun@gmail.com>
2026-06-01 21:22:36 +0300

    Add game logic

--- soft-skills-iw-2-gameoflife.cpp
+++ soft-skills-iw-2-gameoflife.cpp
@@ -1,5 +1,11 @@
-﻿#include <iostream>
+﻿#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <sstream>
+
 using namespace std;
+
+const float CELL_SIZE = 24.f;   // розмір однієї клітини (в пікселях)
+const float DELAY = 0.2f;       // інтервал між ігровими тіками (в секундах)
 
 class Cells {
 private:
@@ -10,8 +16,7 @@ private:
     // якщо клітина жива, повертається одиниця; інакше - нуль
 
     int GetCellStateForCount(int i, int j) const {
-        bool state = GetCell(i, j);
-        return state ? 1 : 0;
+        return GetCell(i, j) ? 1 : 0;
     }
 public:
     Cells(int width, int height) {
@@ -93,5 +98,100 @@ public:
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int width = 0;
+    int height = 0;
+
+    cout << "Enter window width (number of cells) = ";
+    cin >> width;
+
+    cout << "Enter window height (number of cells) = ";
+    cin >> height;
+
+    Cells cells(width, height);
+
+    cout << "Enter cell coordinates (like \"2,3|6,4|12,11\"\n";
+
+    string input;
+    cin >> input;
+    istringstream stream(input);
+    string token;
+
+    while (getline(stream, token, '|')) {
+        try {
+            int x = stoi(token.substr(0, token.find_first_of(','))) - 1;
+            int y = stoi(token.substr(token.find_first_of(',') + 1)) - 1;
+
+            cells.SetCell(y, x, true);
+        }
+        catch (...) {
+            cout << "Invalid input!\n";
+            return 0;
+        }
+    }
+
+    sf::RenderWindow window(
+        sf::VideoMode(
+            {
+                static_cast<unsigned int>(CELL_SIZE * width),
+                static_cast<unsigned int>(CELL_SIZE * height)
+            }
+        ),
+        "Game of life",
+        sf::Style::Titlebar | sf::Style::Close
+    );
+    sf::Clock clock;
+
+    sf::RectangleShape cellShape(sf::Vector2f(CELL_SIZE, CELL_SIZE));
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear(sf::Color(225, 225, 225));
+
+        // рендер поточного стану клітин
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (cells.GetCell(i, j)) {
+                    cellShape.setOrigin({ 0.f, 0.f });
+                    cellShape.setPosition({ j * CELL_SIZE, i * CELL_SIZE });
+                    cellShape.setFillColor(sf::Color::Black);
+
+                    window.draw(cellShape);
+                }
+            }
+        }
+
+        // оновлення стану клітин
+
+        if (clock.getElapsedTime().asSeconds() >= DELAY) {
+            Cells tempCells(cells);
+
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    int numOfNeighbors = cells.GetNumberOfNeighbors(i, j);
+
+                    if (cells.GetCell(i, j) && (numOfNeighbors < 2 || numOfNeighbors > 3)) {
+                        tempCells.SetCell(i, j, false);
+                    }
+                    else if (!cells.GetCell(i, j) && numOfNeighbors == 3) {
+                        tempCells.SetCell(i, j, true);
+                    }
+                }
+            }
+
+            cells = tempCells;
+
+            clock.restart();
+        }
+
+        window.display();
+    }
+
+    return 0;
 }
